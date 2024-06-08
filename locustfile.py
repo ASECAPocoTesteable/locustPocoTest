0f519595fc7354b01ad8c193a627814c15d539a1
Gonza69420 <gonzalo.mato@ing.austral.edu.ar>
2024-06-08 19:56:26 -0300

    locust fix.

--- locustfile.py
+++ locustfile.py
@@ -4,41 +4,34 @@ import random
 import string
 
 
-# class ShopUser(HttpUser):
-#     host = 'http://localhost:8080'
-#     wait_time = between(1, 5)
-#
-#     @task
-#     def get_items(self):
-#         self.client.get("/shop")
-#
-#     @task
-#     def add_product(self):
-#         name = ''.join(random.choices(string.ascii_letters, k=10))
-#         price = random.randint(1, 1000)
-#         self.client.post("/shop/product/add", json={"name": name, "price": price})
-#
-#     @task
-#     def checkout(self):
-#         products = [{"productId": 1, "quantity": random.randint(1, 10)} for _ in range(5)]
-#         direction = "North"
-#         create_order_request = {"products": products, "direction": direction}
-#         self.client.post("/order/checkout", json=create_order_request, headers={"Content-Type": "application/json"})
-#
-#
-# class BrowseUser(HttpUser):
-#     host = 'http://localhost:8080'
-#     wait_time = between(1, 5)
-#
-#     @task
-#     def view_products(self):
-#         self.client.get("/shop")
-#
-#     @task
-#     def view_single_product(self):
-#         product_id = random.randint(1, 100)
-#         self.client.get(f"/product/getById?id={product_id}")
-#
+class ShopUser(HttpUser):
+    host = 'http://localhost:8080'
+    wait_time = between(1, 5)
+
+    @task
+    def get_items(self):
+        self.client.get("/shop")
+
+    @task
+    def add_product(self):
+        name = ''.join(random.choices(string.ascii_letters, k=10))
+        price = random.randint(1, 1000)
+        self.client.post("/shop/product/add", json={"name": name, "price": price})
+
+
+class BrowseUser(HttpUser):
+    host = 'http://localhost:8080'
+    wait_time = between(1, 5)
+
+    @task
+    def view_products(self):
+        self.client.get("/shop")
+
+    @task
+    def view_single_product(self):
+        product_id = random.randint(1, 100)
+        self.client.get(f"/product/getById?id={product_id}")
+
 
 class CheckoutUser(HttpUser):
     host = 'http://localhost:8080'
@@ -46,13 +39,12 @@ class CheckoutUser(HttpUser):
 
     @task
     def checkout_process(self):
-        productId = random.randint(1, 1000)
+        productId = random.randint(1, 20)
         products = [{"productId": productId, "quantity": random.randint(1, 10)} for _ in range(5)]
         direction = "South"
         create_order_request = {"products": products, "direction": direction}
         response = self.client.post("/order/checkout", json=create_order_request,
                                     headers={"Content-Type": "application/json"})
-
         if response.status_code != 200:
             self.on_failure()
 
@@ -61,26 +53,15 @@ class CheckoutUser(HttpUser):
 
     def add_products(self):
         name = ''.join(random.choices(string.ascii_letters, k=10))
-        price = random.randint(1, 1000)
+        price = random.randint(1, 20)
         response = self.client.post("/shop/product/add", json={"name": name, "price": price})
         if response.status_code == 200:
-            response_data = response.json()
-            product_id = response_data.get("id")
-            if product_id:  # Ensure the response contains an 'id'
-                self.add_stock(product_id)
+            self.add_stock()
 
-    def add_stock(self, product_id):
+    def add_stock(self):
+        product_id = random.randint(1, 20)
         self.client.base_url = 'http://localhost:8081'
-        response = self.client.put(f"/product/{product_id}",
-                                   json={"id": product_id, "addedQuantity": random.randint(1, 20)},
-                                   headers={"Content-Type": "application/json"})
+        self.client.put(f"/product/{product_id}",
+                        json={"id": product_id, "addedQuantity": random.randint(20, 100)},
+                        headers={"Content-Type": "application/json"})
         self.client.base_url = self.host
-        if response.status_code == 200:
-            self.retry_checkout(product_id)
-
-    def retry_checkout(self, product_id):
-        products = [{"productId": product_id, "quantity": random.randint(1, 10)} for _ in range(5)]
-        direction = "South"
-        create_order_request = {"products": products, "direction": direction}
-        self.client.post("/order/checkout", json=create_order_request,
-                         headers={"Content-Type": "application/json"})
